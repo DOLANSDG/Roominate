0b469cd47bba3b33c7a643a7192700b43c318724
Lauren Hartley <lahartley99@gmail.com>
2022-03-25 18:30:22 -0400

    Added canvas, made canvas fullscreen

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let editSideActive = false; // Check whether editor sidebar is displayed
 let shapeSideActive = false; // Check whether object sidebar is displayed
+let canvas = new fabric.Canvas('canvas');
 
 // Quick helper function to get HTML elements
 var $ = function(id) {return document.getElementById(id)};
@@ -24,3 +25,16 @@ function toggleShapeSidebar() {
         shapeSideActive = true;
     }
 }
+
+// Makes the canvas fullscreen
+(function() {
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+    function resizeCanvas() {
+            canvas.setWidth(window.innerWidth);
+            canvas.setHeight(window.innerHeight);
+            canvas.renderAll();
+    }
+    
+    resizeCanvas();
+})();
