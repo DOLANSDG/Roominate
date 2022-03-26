1d8c7d014959b50caef6273d8d6f513f1731be59
Lauren Hartley <lahartley99@gmail.com>
2022-03-26 18:36:27 -0400

    Added canvas zoom and pan

--- scripts.js
+++ scripts.js
@@ -26,6 +26,7 @@ function toggleShapeSidebar() {
     }
 }
 
+// Basic Canvas Functions //////////////////////////////////
 // Makes the canvas fullscreen
 (function() {
     // resize the canvas to fill browser window dynamically
@@ -38,3 +39,42 @@ function toggleShapeSidebar() {
     
     resizeCanvas();
 })();
+
+// Canvas Zoom and Pan
+canvas.on('mouse:wheel', function(opt) {
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom *= 0.999 ** delta;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+});
+canvas.on('mouse:down', function(opt) {
+    var evt = opt.e;
+    if (evt.altKey === true) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+    }
+});
+canvas.on('mouse:move', function(opt) {
+    if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+    }
+});
+canvas.on('mouse:up', function(opt) {
+    // on mouse up we want to recalculate new interaction
+    // for all objects, so we call setViewportTransform
+    this.setViewportTransform(this.viewportTransform);
+    this.isDragging = false;
+    this.selection = true;
+});
