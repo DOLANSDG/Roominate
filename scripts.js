eae327b6e38ff4d910867a2f1c40c0f8384d64c2
Lauren Hartley <lahartley99@gmail.com>
2022-03-30 16:05:50 -0400

    Send grid lines to back on grid creation

--- scripts.js
+++ scripts.js
@@ -191,8 +191,8 @@ let grid = gridCreator();
 function toggleGrid() {
     for (let i = 0; i < grid.length; i++) {
         gridActive ?  canvas.remove(grid[i]) : canvas.add(grid[i]);
+        if (gridActive) canvas.sendToBack(grid[i]); // On grid creation, send to back to prevent object behind grid
     }
-
     gridActive = gridActive ? false : true;
 }
 
