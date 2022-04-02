0d119d988f9c28118ea76186243163fc97624b06
Lauren Hartley <lahartley99@gmail.com>
2022-04-01 23:06:05 -0400

    polygon lines able to draw on screen

--- scripts.js
+++ scripts.js
@@ -29,6 +29,21 @@ function centerCoord(){
     }
 }
 
+/**
+ * Point class
+ */
+class Point {
+    /**
+     * Create a point.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 /* -------------------------------------------------------------------------- */
 /*                              Button Functions                              */
 /* -------------------------------------------------------------------------- */
@@ -198,182 +213,6 @@ function toggleGrid() {
     gridActive = gridActive ? false : true;
 }
 
-var roomButton = $('room-icon');
-roomButton.onclick = function() {
-    roomMode = true;
-};
-
-// canvas.on('mouse:down', function (options) {
-//     if (roomMode){
-//         prototypefabric.polygon.addPoint(options);
-
-//     }
-// });
-
-var min = 99;
-var max = 999999;
-var polygonMode = true;
-var pointArray = new Array();
-var lineArray = new Array();
-var activeLine;
-var activeShape = false;
-
-window.onload = function(){
-    prototypefabric.initControls();
-};
-
-
-var prototypefabric = new function () {
-    this.initControls = function () {
-        canvas.on('mouse:down', function (options) {
-            if(options.target && options.target.id == pointArray[0].id){
-                prototypefabric.polygon.generatePolygon(pointArray);
-            }
-            if(polygonMode){
-                prototypefabric.polygon.addPoint(options);
-            }
-        });
-        canvas.on('mouse:up', function (options) {
-
-        });
-        canvas.on('mouse:move', function (options) {
-            if(activeLine && activeLine.class == "line"){
-                var pointer = canvas.getPointer(options.e);
-                activeLine.set({ x2: pointer.x, y2: pointer.y });
-
-                var points = activeShape.get("points");
-                points[pointArray.length] = {
-                    x:pointer.x,
-                    y:pointer.y
-                }
-                activeShape.set({
-                    points: points
-                });
-                canvas.renderAll();
-            }
-            canvas.renderAll();
-        });
-    };
-};
-
-prototypefabric.polygon = {
-    drawPolygon : function() {
-        polygonMode = true;
-        pointArray = new Array();
-        lineArray = new Array();
-        activeLine;
-    },
-    addPoint : function(options) {
-        var random = Math.floor(Math.random() * (max - min + 1)) + min;
-        var id = new Date().getTime() + random;
-        var circle = new fabric.Circle({
-            radius: 5,
-            fill: '#ffffff',
-            stroke: '#333333',
-            strokeWidth: 0.5,
-            left: (options.e.layerX/canvas.getZoom()),
-            top: (options.e.layerY/canvas.getZoom()),
-            selectable: false,
-            hasBorders: false,
-            hasControls: false,
-            originX:'center',
-            originY:'center',
-            id:id
-        });
-        if(pointArray.length == 0){
-            circle.set({
-                fill:'red'
-            })
-        }
-        var points = [(options.e.layerX/canvas.getZoom()),(options.e.layerY/canvas.getZoom()),(options.e.layerX/canvas.getZoom()),(options.e.layerY/canvas.getZoom())];
-        line = new fabric.Line(points, {
-            strokeWidth: 2,
-            fill: '#999999',
-            stroke: '#999999',
-            class:'line',
-            originX:'center',
-            originY:'center',
-            selectable: false,
-            hasBorders: false,
-            hasControls: false,
-            evented: false
-        });
-        if(activeShape){
-            var pos = canvas.getPointer(options.e);
-            var points = activeShape.get("points");
-            points.push({
-                x: pos.x,
-                y: pos.y
-            });
-            var polygon = new fabric.Polygon(points,{
-                stroke:'#333333',
-                strokeWidth:1,
-                fill: '#cccccc',
-                opacity: 0.1,
-                selectable: false,
-                hasBorders: false,
-                hasControls: false,
-                evented: false
-            });
-            canvas.remove(activeShape);
-            canvas.add(polygon);
-            activeShape = polygon;
-            canvas.renderAll();
-        }
-        else{
-            var polyPoint = [{x:(options.e.layerX/canvas.getZoom()),y:(options.e.layerY/canvas.getZoom())}];
-            var polygon = new fabric.Polygon(polyPoint,{
-                stroke:'#333333',
-                strokeWidth:1,
-                fill: '#cccccc',
-                opacity: 0.1,
-                selectable: false,
-                hasBorders: false,
-                hasControls: false,
-                evented: false
-            });
-            activeShape = polygon;
-            canvas.add(polygon);
-        }
-        activeLine = line;
-
-        pointArray.push(circle);
-        lineArray.push(line);
-
-        canvas.add(line);
-        canvas.add(circle);
-        canvas.selection = false;
-    },
-    generatePolygon : function(pointArray){
-        var points = new Array();
-        pointArray.forEach(function(point,index){
-            points.push({
-                x:point.left,
-                y:point.top
-            });
-            canvas.remove(point);
-        });
-        // $.each(lineArray,function(index,line){
-        //     canvas.remove(line);
-        // });
-        canvas.remove(activeShape).remove(activeLine);
-        var polygon = new fabric.Polygon(points,{
-            stroke:'#333333',
-            strokeWidth:0.5,
-            fill: 'red',
-            opacity: 1,
-            hasBorders: false,
-            hasControls: false
-        });
-        canvas.add(polygon);
-
-        activeLine = null;
-        activeShape = null;
-        polygonMode = false;
-        canvas.selection = true;
-    }
-};
-
 /**
 * Resizes the canvas  when brower size is adjusted, make the canvas fullscreen
 */
@@ -446,6 +285,77 @@ function createPoly() {
     })
     canvas.add(poly);
     canvas.setActiveObject(poly);
+}
+
+var roomButton = $('room-icon');
+var points = [];
+var lines = [];
+var lineCount = 0;
+var x = 0;
+var y = 0;
+
+roomButton.onclick = function () {
+    console.log("test");
+    if (!roomMode) {
+        roomMode = true;
+    }
+};
+
+canvas.on('mouse:down', function (options) {
+    if (roomMode) {
+        var circle = new fabric.Circle({
+            radius: 5,
+            fill: '#ffffff',
+            stroke: '#333333',
+            strokeWidth: 0.5,
+            left: (options.e.layerX/canvas.getZoom()),
+            top: (options.e.layerY/canvas.getZoom()),
+            selectable: false,
+            hasBorders: false,
+            hasControls: false,
+            originX:'center',
+            originY:'center',
+        });
+        canvas.selection = false;
+        setStartingPoint(options);
+        points.push(new Point(x, y));
+        var point = [x,y,x,y];
+        var line = new fabric.Line(point, {
+            strokeWidth: 4,
+            fill: '#999999',
+            stroke: 'red',
+            class:'line',
+            originX:'center',
+            originY:'center',
+            selectable: false,
+            hasBorders: false,
+            hasControls: false,
+            evented: false
+        });
+        lines.push(line);
+        canvas.add(lines[lineCount]);
+        canvas.add(circle);
+        lineCount++;
+        canvas.on('mouse:up', function (options) {
+            canvas.selection = true;
+        });
+    }
+});
+canvas.on('mouse:move', function (options) {
+    if (lines[0] !== null && lines[0] !== undefined && roomMode) {
+        setStartingPoint(options);
+        lines[lineCount - 1].set({
+            x2: x,
+            y2: y
+        });
+        canvas.renderAll();
+    }
+});
+
+function setStartingPoint(options) {
+    var pointer = canvas.getPointer();
+    x = pointer.x;
+    y = pointer.y;
 }
 
 /* -------------------------------------------------------------------------- */
