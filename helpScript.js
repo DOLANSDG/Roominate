1f194d9eb4c304f2a79d799b4b34bbe2d27bd8f7
Olivia Leonard <olivialeonard@Olivias-MacBook-Air.local>
2022-04-13 15:16:26 -0400

    working on style for pages

--- helpScript.js
+++ helpScript.js
@@ -1,17 +1,16 @@
 // referenced https://sweetcode.io/how-to-build-an-faq-page-with-html-and-javascript/ to help create drop down menu
 
-var faq = document.getElementsByClassName("question");
+var acc = document.getElementsByClassName("question");
 var i;
 
-for (i = 0; i < question.length; i++) {
-    question[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = none;
-        } else {
-            body.style.display = "block";
-        }
-    });
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
